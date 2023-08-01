a60787286e30561dc77a6214373b9d8136955081
sharonpjoseph <sharon.joseph@techjays.com>
2023-08-01 13:30:13 +0530

    uqery params added

--- lib/data/network_service/rest_client.dart
+++ lib/data/network_service/rest_client.dart
@@ -1,13 +1,9 @@
-
-
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 import 'package:test_flutter/data/SharedPreferencesHelper.dart';
 import 'package:test_flutter/data/models/api_response.dart';
 import 'package:test_flutter/presentation/utils/utils.dart';
 
 import '../../presentation/constants/constants.dart';
-
 
 class RestClient {
   static final RestClient _instance = RestClient._internal();
@@ -32,7 +28,7 @@ class RestClient {
         headers['platform'] = Utils().getPlatformName();
         SharedPreferencesHelper.init();
         headers['Authorization'] =
-            "Bearer ${SharedPreferencesHelper.getDummyToken()}";
+        "Bearer ${SharedPreferencesHelper.getDummyToken()}";
         print('Request URl: ${options.method} ${options.uri}');
         print('Request Body: $body');
         if (queryParams != null && queryParams.isNotEmpty) {
@@ -66,8 +62,14 @@ class RestClient {
     );
   }
 
-  String _constructUrl(String endpoint) {
-    return '$BASE_URL$endpoint';
+  String _constructUrl(String endpoint,
+      {Map<String, dynamic>? queryParameters}) {
+    String url = BASE_URL + endpoint;
+    if (queryParameters != null && queryParameters.isNotEmpty) {
+      url += '?';
+      url += Uri(queryParameters: queryParameters).query;
+    }
+    return url;
   }
 
   Future<ApiResponse<T>> postRequest<T>({
@@ -83,58 +85,105 @@ class RestClient {
     }
   }
 
-  Future<ApiResponse<T>> getRequest<T>({
+  Future<ApiResponse<T>> putRequest<T>({
     required String endpoint,
+    required Map<String, dynamic> data,
     required T Function(Map<String, dynamic>) fromJson,
+    Map<String, dynamic>? queryParameters,
   }) async {
     try {
-      final response = await _dio.get(_constructUrl(endpoint));
-      debugPrint('API Response Status Code: ${response.statusCode}');
-      debugPrint('API Response Body: ${response.data}');
+      final response = await _dio.put(_constructUrl(endpoint),
+          data: data, queryParameters: queryParameters);
       return handleResponse<T>(response, fromJson);
     } catch (error) {
-      print('API Error: $error');
       return handleErrorResponse<T>(error);
     }
   }
 
-  ApiResponse<T> handleResponse<T>(
-    Response<dynamic> response,
-    T Function(Map<String, dynamic>) fromJson,
-  ) {
-    if (response.statusCode == 200) {
-      return ApiResponse<T>(
-        data: fromJson(response.data),
-        result: true,
-        msg: "success",
-        isNextLink: true,
-      );
-    } else {
-      return ApiResponse<T>(
-        result: false,
-        data: response.data as T,
-        msg: "Request failed with status code ${response.statusCode}",
-        statusCode: response.statusCode,
-      );
+  Future<ApiResponse<T>> deleteRequest<T>({
+    required String endpoint,
+    required Map<String, dynamic> data,
+    required T Function(Map<String, dynamic>) fromJson,
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    try {
+      final response = await _dio.delete(_constructUrl(endpoint),
+          data: data, queryParameters: queryParameters);
+      return handleResponse<T>(response, fromJson);
+    } catch (error) {
+      return handleErrorResponse<T>(error);
     }
   }
 
-  ApiResponse<T> handleErrorResponse<T>(dynamic error) {
-    if (error is DioException && error.response != null) {
-      print(error.response);
-      return ApiResponse<T>(
-        result: false,
-        data: null,
-        msg: "${error.response!.statusCode}/${error.response!.data['message']}",
-        statusCode: error.response!.statusCode!,
-        responseData: error.response!.data,
-      );
-    } else {
-      return ApiResponse<T>(
-        result: false,
-        msg: "Error occurred",
-        data: null,
-      );
+  Future<ApiResponse<T>> getRequest<T>({
+    required String endpoint,
+    required T Function(Map<String, dynamic>) fromJson,
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    try {
+      final response = await _dio.get(_constructUrl(endpoint),
+          queryParameters: queryParameters);
+      return handleResponse<T>(response, fromJson);
+    } catch (error) {
+      return handleErrorResponse<T>(error);
     }
   }
+
+  Future<Response> uploadFileWithProgress(String filePath,
+      String endpoint,
+      Function(int sentBytes, int totalBytes) progressCallback,) async {
+    final url = _constructUrl(endpoint);
+
+    final formData = FormData.fromMap({
+      'file': await MultipartFile.fromFile(filePath),
+    });
+
+    final response = await _dio.post(
+      url,
+      data: formData,
+      onSendProgress: (sentBytes, totalBytes) {
+        progressCallback(sentBytes, totalBytes);
+      },
+    );
+
+    return response;
+  }
 }
+
+ApiResponse<T> handleResponse<T>(Response<dynamic> response,
+    T Function(Map<String, dynamic>) fromJson,) {
+  if (response.statusCode == 200) {
+    return ApiResponse<T>(
+      data: fromJson(response.data),
+      result: true,
+      msg: "success",
+      isNextLink: true,
+    );
+  } else {
+    return ApiResponse<T>(
+      result: false,
+      data: response.data as T,
+      msg: "Request failed with status code ${response.statusCode}",
+      statusCode: response.statusCode,
+    );
+  }
+}
+
+ApiResponse<T> handleErrorResponse<T>(dynamic error) {
+  if (error is DioException && error.response != null) {
+    print(error.response);
+    return ApiResponse<T>(
+      result: false,
+      data: null,
+      msg: "${error.response!.statusCode}/${error.response!.data['message']}",
+      statusCode: error.response!.statusCode!,
+      responseData: error.response!.data,
+    );
+  } else {
+    return ApiResponse<T>(
+      result: false,
+      msg: "Error occurred",
+      data: null,
+    );
+  }
+}}
--- lib/data/services/product_services.dart
+++ lib/data/services/product_services.dart
@@ -1,18 +1,17 @@
-
-
 import '../models/api_response.dart';
 import '../models/products_model.dart';
 import '../network_service/rest_client.dart';
-
 
 class ProductServices {
   String products = 'products';
 
   Future<ApiResponse<ProductsModel>> getProducts() async {
+    var qP = {'param': '123', 'param2': 'abcd'};
     var dioClient = RestClient();
     return dioClient.getRequest<ProductsModel>(
       endpoint: products,
       fromJson: (json) => ProductsModel.fromJson(json),
+      queryParameters: null
     );
   }
 }
